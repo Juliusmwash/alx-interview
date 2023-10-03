7a2fcb5402cdeb28199015b5bf0854b15ca7ab9c
Juliusmwash <cutejuliusmwash@gmail.com>
2023-10-03 16:29:05 +0300

    Add explanative docstrings to all functions

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,9 +6,30 @@ This module defines the canUnlockAll function
 
 def canUnlockAll(boxes):
     """
-    Recursive function for finding keys that can open the locked boxes
+    Determine if all locked boxes can be opened using their keys.
+
+    Args:
+        boxes (list of list): A list of boxes, where each box
+        contains keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
     """
     def dfs(box_index, visited, n):
+        """
+        Perform depth-first search (DFS) to explore boxes
+        and mark them as visited.
+
+        Args:
+            box_index (int): The index of the current box being explored.
+            visited (list of bool): A list indicating whether each
+            box has been visited.
+
+            n (int): The total number of boxes.
+
+        Returns:
+            None
+        """
         if box_index < n:
             if not boxes[box_index]:
                 visited[box_index] = True
