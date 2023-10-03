da84fb2d891f1e57af235b75167d00e1dff05294
Juliusmwash <cutejuliusmwash@gmail.com>
2023-10-03 15:12:17 +0300

    Add an if condition to check if a key represents a box or not

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,13 +8,13 @@ def canUnlockAll(boxes):
     """
     Recursive function for finding keys that can open the locked boxes
     """
-    def dfs(box_index, visited):
+    def dfs(box_index, visited, n):
         visited[box_index] = True
         for key in boxes[box_index]:
-            if not visited[key]:
-                dfs(key, visited)
+            if key < n and not visited[key]:
+                dfs(key, visited, n)
 
     n = len(boxes)
     visited = [False] * n
-    dfs(0, visited)
+    dfs(0, visited, n)
     return all(visited)
