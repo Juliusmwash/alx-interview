7365c1f1a61440baf36b7ebc517a9457925508b8
Juliusmwash <cutejuliusmwash@gmail.com>
2023-10-06 00:50:25 +0300

    Fix a troublesome bug

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module defines the canUnlockAll function
+This module defines a function 'canUnlockAll'.
 """
 
 
@@ -9,41 +9,25 @@ def canUnlockAll(boxes):
     Determine if all locked boxes can be opened using their keys.
 
     Args:
-        boxes (list of list): A list of boxes, where each box
-        contains keys to other boxes.
+        boxes (list of list): A list of boxes, where each box contains
+        keys to other boxes.
 
     Returns:
         bool: True if all boxes can be opened, False otherwise.
     """
-    def dfs(box_index, visited, n):
-        """
-        Perform depth-first search (DFS) to explore boxes
-        and mark them as visited.
-
-        Args:
-            box_index (int): The index of the current box being explored.
-            visited (list of bool): A list indicating whether each
-            box has been visited.
-
-            n (int): The total number of boxes.
-
-        Returns:
-            None
-        """
-        for box in boxes:
-            if type(box) is not list:
-                return False
-        if box_index < n:
-            if not boxes[box_index]:
-                visited[box_index] = True
-                dfs(box_index + 1, visited, n)
-            else:
-                visited[box_index] = True
-            for key in boxes[box_index]:
-                if key < n and not visited[key]:
-                    dfs(key, visited, n)
+    if not boxes:
+        return False
 
     n = len(boxes)
     visited = [False] * n
-    dfs(0, visited, n)
+    visited[0] = True
+
+    def dfs(box_index):
+        visited[box_index] = True
+        for key in boxes[box_index]:
+            if key < n and not visited[key]:
+                dfs(key)
+
+    dfs(0)
+
     return all(visited)
