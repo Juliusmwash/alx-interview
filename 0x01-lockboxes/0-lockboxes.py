9d12b381fb9bf7a29dd492803728301f5bf89f60
Juliusmwash <cutejuliusmwash@gmail.com>
2023-10-06 17:26:54 +0300

    Try to solve the checker error for 1000 boxes with each box having all the keys to all the other boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,14 +3,12 @@
 This module defines a function 'canUnlockAll'.
 """
 
-
 def canUnlockAll(boxes):
     """
     Determine if all locked boxes can be opened using their keys.
 
     Args:
-        boxes (list of list): A list of boxes, where each box contains
-        keys to other boxes.
+        boxes (list of list): A list of boxes, where each box contains keys to other boxes.
 
     Returns:
         bool: True if all boxes can be opened, False otherwise.
@@ -23,11 +21,24 @@ def canUnlockAll(boxes):
     visited[0] = True
 
     def dfs(box_index):
-        visited[box_index] = True
+        """
+        Recursive function to perform depth-first search (DFS) to unlock boxes.
+
+        Args:
+            box_index (int): Index of the current box.
+
+        Returns:
+            bool: True if all boxes can be opened, False otherwise.
+        """
+        if all(visited):
+            return True  # All boxes are open, return True
+
         for key in boxes[box_index]:
             if key < n and not visited[key]:
-                dfs(key)
+                visited[key] = True
+                if dfs(key):
+                    return True
 
-    dfs(0)
+        return False
 
-    return all(visited)
+    return dfs(0)
