89225bf37657444ae81e32cea7051e40488bc19d
Juliusmwash <cutejuliusmwash@gmail.com>
2023-11-22 14:26:30 +0300

    Commit second version of the file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,41 +1,63 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-Task: Change comes from within
-Given a pile of coins of different values,
-determine the fewest number of coins needed to
-meet a given amount total
+Coin Change Problem Solver
+
+This script provides a function to determine the fewest number of coins
+needed to meet a given amount total, given a list of coin denominations.
+
+Usage:
+    makeChange(coins, total)
+
+Parameters:
+    coins (list): List of coin values.
+    total (int): Target amount to make change for.
+
+Returns:
+    int: The fewest number of coins needed to meet the total.
+         Returns -1 if the total cannot be met by any combination of coins.
+
+Example:
+    >>> makeChange([1, 2, 25], 37)
+    7
+    >>> makeChange([1256, 54, 48, 16, 102], 1453)
+    -1
 """
+import sys
 
 
 def makeChange(coins, total):
     """
-    Given a pile of coins of different values, determine the fewest
-    number of coins needed to meet a given amount total.
-    Return: fewest number of coins needed to meet total
-        - If total is 0 or less, return 0
-        - If total cannot be met by any number of coins you have, return -1
-        - Coins is a list of the values of the coins in your possession
-        - The value of a coin will always be an integer greater than 0
-        - You can assume you have an infinite number of each denomination of
-        coin in the list
+    Calculate the fewest number of coins needed to meet the given
+    amount total.
+
+    Args:
+        coins (list): List of coin values.
+        total (int): Target amount to make change for.
+
+    Returns:
+        int: The fewest number of coins needed to meet the total.
+             Returns -1 if the total cannot be met by any combination
+             of coins.
     """
-    if total <= 0:
-        return 0
+    n = len(coins)
+    dp = [sys.maxsize] * (total + 1)
+    dp[0] = 0
 
-    newVal = total + 1
-    store = {0: 0}
+    for amt in range(1, total + 1):
+        temp = sys.maxsize
+        for c in range(n):
+            if coins[c] <= amt:
+                temp_amt = dp[amt - coins[c]] + 1
+                if temp_amt < temp:
+                    temp = temp_amt
+                    dp[amt] = temp
 
-    for i in range(1, total + 1):
-        store[i] = newVal
+    return dp[total] if dp[total] != sys.maxsize else -1
 
-        for coin in coins:
-            current = i - coin
-            if current < 0:
-                continue
 
-            store[i] = min(store[current] + 1, store[i])
-
-    if store[total] == total + 1:
-        return -1
-
-    return store[total]
+"""
+# Test cases
+if __name__ == "__main__":
+    print(makeChange([1, 2, 25], 37))   # Output: 7
+    print(makeChange([1256, 54, 48, 16, 102], 1453))   # Output: -1
+"""
